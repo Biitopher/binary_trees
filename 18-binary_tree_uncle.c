a5af8af90664b6fb4652f52c72248945835b11b7
Evans Sese <sese_evans@yahoo.com>
2023-08-30 08:34:55 -0700

    [Feature] Task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - Finds uncle to a node
+ * @node: Node
+ * Return: The uncle node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *grandparent = node->parent->parent;
+
+	if (node == NULL || node->parent == NULL || grandparent == NULL)
+		return (NULL);
+
+	if (grandparent->left == node->parent)
+		return (grandparent->right);
+	else if (grandparent->right == node->parent)
+		return (grandparent->left);
+	else
+		return (NULL);
+}
