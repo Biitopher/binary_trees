b1496287fe2b1e2a70d9c8f702bfa204d976303d
Evans Sese <sese_evans@yahoo.com>
2023-08-30 07:20:57 -0700

    [Feature] Task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - Finds the balance of a binary tree
+ * @tree: Pointer to the tree
+ * Return: Returns the balance factor
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = (int)binary_tree_height(tree->left);
+	right_height = (int)binary_tree_height(tree->right);
+
+	return (left_height - right_height);
+}
+
+/**
+ * binary_tree_height - gets the height of the tree
+ * @tree: Pointer to the tree
+ * Return: Returns height of tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+	
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (left_height > right_height ? left_height + 1 : right_height + 1);
+}
