5cd6e1987ee2fce96d3a50f8b69754f4121201ce
Evans Sese <sese_evans@yahoo.com>
2023-08-30 08:41:02 -0700

    [Feature] Task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -13,8 +13,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 
 	if (grandparent->left == node->parent)
 		return (grandparent->right);
-	else if (grandparent->right == node->parent)
-		return (grandparent->left);
-	else
-		return (NULL);
+	return (grandparent->left);
 }
