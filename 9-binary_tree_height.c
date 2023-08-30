ea345b39340036ba05fcfadef5bcd20400eeb76c
Evans Sese <sese_evans@yahoo.com>
2023-08-30 03:26:36 -0700

    [Fix] Account for root node

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,19 +1,28 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_height - gets the height of the tree
+ * @tree: Pointer to the tree
+ * Return: Returns height of tree
+ */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height, height;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
+	if (tree->left == NULL)
+		left_height = 0;
+	else
+		left_height = binary_tree_height(tree->left) + 1;
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	if (tree->right == NULL)
+		right_height = 0;
+	else
+		right_height = binary_tree_height(tree->right) + 1;
 
 	if (left_height > right_height)
-		height = left_height + 1;
+		return (left_height);
 	else
-		height = right_height + 1;
-
-	return (height);
+		return (right_height);
 }
