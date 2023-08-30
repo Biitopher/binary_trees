4aa59f7b6ed901df1134d9c9038768107b229184
Evans Sese <sese_evans@yahoo.com>
2023-08-30 02:39:30 -0700

    [Feature] Task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height, height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		height = left_height + 1;
+	else
+		height = right_height + 1;
+
+	return (height);
+}
