c80ef5a2ec95e0389883f7c4e45ea26cfae650fb
Biitopher <biitopher@gmail.com>
2023-08-30 11:25:17 -0700

    [Feature]Task 19

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: pointer to first node
+ * @second: pointer to second node
+ * Return: pointer to lowest common ancestor otherwise NULL
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	size_t depth_first = depth(first);
+	size_t depth_second = depth(second);
+
+	while (depth_first > depth_second)
+	{
+		first = first->parent;
+		depth_first--;
+	}
+	while (depth_second > depth_first)
+	{
+		second = second->parent;
+		depth_second--;
+	}
+
+	while (first && second)
+	{
+		if (first == second)
+			return ((binary_tree_t *)first);
+
+		first = first->parent;
+		second = second->parent;
+	}
+
+	return (NULL);
+}
