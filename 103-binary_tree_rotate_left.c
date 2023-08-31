74245caa55ba8c2201d8c56f96fc15da133ecf9e
Biitopher <biitopher@gmail.com>
2023-08-31 10:11:21 -0700

    [Fix]Task 22

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include "queue.h"
 
 /**
  * binary_tree_rotate_left - performs a left-rotation on a binary tree
@@ -14,6 +15,7 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 	}
 
 	binary_tree_t *new_root = tree->right;
+
 	tree->right = new_root->left;
 	new_root->left = tree;
 
