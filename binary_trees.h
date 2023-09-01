1efde07b0d1b35fe374afe5779a9862acad50b73
Biitopher <biitopher@gmail.com>
2023-09-01 05:46:06 -0700

    [Fix]header

--- binary_trees.h
+++ binary_trees.h
@@ -58,7 +58,6 @@ bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
-bst_t* find_min(bst_t* node);
 void binary_tree_print(const binary_tree_t *);
 
 #endif /* BINARY_TREES_H */
