04e8b6cd9a5a7f53445a46e68d20997ae75973b7
Evans Sese <sese_evans@yahoo.com>
2023-09-01 13:36:06 -0700

    [Fix] Betty

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -3,61 +3,78 @@
 bst_t *bst_find_min(bst_t *node);
 bst_t *bst_remove_recursive(bst_t *root, int value);
 
+/**
+ * bst_remove - removes a node from a Binary Search Tree
+ * @root: pointer to root node of tree where you remove node
+ * @value: value to remove from tree
+ * Return: pointer to new node
+ */
 bst_t *bst_remove(bst_t *root, int value)
 {
-    if (root == NULL)
-        return NULL;
+	if (root == NULL)
+		return (NULL);
 
-    return bst_remove_recursive(root, value);
+	return (bst_remove_recursive(root, value));
 }
 
+/**
+ * bst_remove_recursive - removes a node from a Binary Search Tree recursively
+ * @root: pointer to root node of tree where you remove node
+ * @value: value to remove from tree
+ * Return: pointer to new node
+ */
 bst_t *bst_remove_recursive(bst_t *root, int value)
 {
 	bst_t *temp;
 
-    if (root == NULL)
-        return root;
+	if (root == NULL)
+		return (root);
 
-    if (value < root->n)
-    {
-        root->left = bst_remove_recursive(root->left, value);
-    }
-    else if (value > root->n)
-    {
-        root->right = bst_remove_recursive(root->right, value);
-    }
-    else
-    {
-        if (root->left == NULL)
-        {
-            bst_t *temp = root->right;
-            free(root);
-            return temp;
-        }
-        else if (root->right == NULL)
-        {
-            bst_t *temp = root->left;
-            free(root);
-            return temp;
-        }
+	if (value < root->n)
+	{
+		root->left = bst_remove_recursive(root->left, value);
+	}
+	else if (value > root->n)
+	{
+		root->right = bst_remove_recursive(root->right, value);
+	}
+	else
+	{
+		if (root->left == NULL)
+		{
+			temp = root->right;
+			free(root);
+			return (temp);
+		}
+		else if (root->right == NULL)
+		{
+			temp = root->left;
+			free(root);
+			return (temp);
+		}
 
-        temp = bst_find_min(root->right);
+		temp = bst_find_min(root->right);
 
-        root->n = temp->n;
-        root->right = bst_remove_recursive(root->right, temp->n);
-    }
+		root->n = temp->n;
+		root->right = bst_remove_recursive(root->right, temp->n);
+	}
 
-    return root;
+	return (root);
 }
 
+/**
+ * bst_find_min - finds minimum value
+ * @node: node being searched
+ * Return: node on success
+ */
 bst_t *bst_find_min(bst_t *node)
 {
-    bst_t *current = node;
+	bst_t *current = node;
 
-    while (current && current->left != NULL)
-    {
-        current = current->left;
-    }
+	while (current && current->left != NULL)
+	{
+		current = current->left;
+	}
 
-    return current;
+	return (current);
 }
