035cc040308aeb0f1108754835962f054525498b
Evans Sese <sese_evans@yahoo.com>
2023-09-01 06:12:06 -0700

    [Fix] Code indentation

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -30,38 +30,38 @@ bst_t *bst_remove(bst_t *root, int value)
 	if (root == NULL)
 	{
 		return (NULL);
-
-		if (value < root->n)
-		{
-			root->left = bst_remove(root->left, value);
-		}
-		else if (value > root->n)
-		{
-			root->right = bst_remove(root->right, value);
-		}
-		else
-		{
-			if (root->left == NULL)
-			{
-				bst_t *temp = root->right;
-
-				free(root);
-				return (temp);
-			}
-			else if (root->right == NULL)
-			{
-				bst_t *temp = root->left;
-
-				free(root);
-				return (temp);
-			}
-
-			temp = find_min(root->right);
-
-			root->n = temp->n;
-			root->right = bst_remove(root->right, temp->n);
-		}
-
 	}
+
+	if (value < root->n)
+	{
+		root->left = bst_remove(root->left, value);
+	}
+	else if (value > root->n)
+	{
+		root->right = bst_remove(root->right, value);
+	}
+	else
+	{
+		if (root->left == NULL)
+		{
+			temp = root->right;
+
+			free(root);
+			return (temp);
+		}
+		else if (root->right == NULL)
+		{
+			temp = root->left;
+
+			free(root);
+			return (temp);
+		}
+
+		temp = find_min(root->right);
+
+		root->n = temp->n;
+		root->right = bst_remove(root->right, temp->n);
+	}
+
 	return (root);
 }
